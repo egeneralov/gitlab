f60a21c261d0a88a05cf81149018af8bd0fddcc1
egeneralov <egeneralov@technotimes.org>
2019-02-15 22:10:31 +0300

    auto

--- files/pat.sql
+++ files/pat.sql
@@ -1,3 +1,42 @@
-COPY public.personal_access_tokens (id, user_id, token, name, revoked, expires_at, created_at, updated_at, scopes, impersonation, token_digest) FROM stdin;
-1	1	\N	terraform	f	\N	2019-01-20 01:24:16.216326	2019-01-20 01:24:16.216326	---\n- api\n- read_user\n- sudo\n- read_repository\n- read_registry\n	f	4eZfo+Qvx/wYCkyT1hHEGGFkDBvRtTwp4nPEWhhA1aQ=
-\.
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.11
+-- Dumped by pg_dump version 9.6.11
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: personal_access_tokens; Type: TABLE DATA; Schema: public; Owner: gitlab
+--
+
+INSERT INTO public.personal_access_tokens VALUES (4, 1, NULL, 'terraform', false, NULL, '2019-02-15 19:08:30.639803', '2019-02-15 19:08:30.639803', '---
+- api
+- read_user
+- sudo
+- read_repository
+- read_registry
+', false, 'Y6QGb5hFzofUaw9VNdY+Z+OuSdluNs1fl/gK8s+qcFM=');
+
+
+--
+-- Name: personal_access_tokens_id_seq; Type: SEQUENCE SET; Schema: public; Owner: gitlab
+--
+
+SELECT pg_catalog.setval('public.personal_access_tokens_id_seq', 4, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
